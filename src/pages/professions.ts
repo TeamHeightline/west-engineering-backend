a5b55c3d286305de925360a469e5b9cbc77c6c71
heightline <teamheightline@mail.ru>
2022-08-11 13:06:07 +0300

    Создал полноценный редактор должностей

--- src/pages/professions.ts
+++ src/pages/professions.ts
@@ -12,4 +12,39 @@ professionsRouter.get("/all", async (req, res) => {
     res.status(200).json(professions)
 })
 
+professionsRouter.post("/create", async (req, res) => {
+    const {name} = req.body
+    const profession = await prisma.pROFESSIONS.create({
+        data: {
+            name
+        }
+    })
+    res.status(200).json(profession)
+})
+
+professionsRouter.post("/delete", async (req, res) => {
+    const {id} = req.body
+    await prisma.pROFESSIONS.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+    res.status(200).json({
+        message: "Profession deleted"
+    })
+})
+
+professionsRouter.post("/update", async (req, res) => {
+    const {id, name} = req.body
+    const updatedProfession = await prisma.pROFESSIONS.update({
+        where: {
+            id: Number(id)
+        },
+        data: {
+            name
+        }
+    })
+    res.status(200).json(updatedProfession)
+})
+
 export default professionsRouter;
