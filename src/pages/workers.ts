541b00244c0c4d44b19c8b8914ca22439a1ab619
heightline <teamheightline@mail.ru>
2022-08-12 13:43:24 +0300

    Сделал добавление и удаление сотрудников

--- src/pages/workers.ts
+++ src/pages/workers.ts
@@ -32,4 +32,26 @@ workersRouter.post('/update', async (req, res) => {
     res.status(200).json(worker)
 })
 
+workersRouter.post('/create', async (req, res) => {
+    const {name, level_of_education_id, profession_id} = req.body
+    const worker = await prisma.wORKER.create({
+        data: {
+            name,
+            level_of_education_id,
+            profession_id,
+        }
+    })
+    res.status(200).json(worker)
+})
+
+workersRouter.post('/delete', async (req, res) => {
+    const {id} = req.body
+    const worker = await prisma.wORKER.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+    res.status(200).json(worker)
+})
+
 export default workersRouter;
